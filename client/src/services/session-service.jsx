d0e8202dc2a239378bc4668b4935a18cc5e871e6
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-07-04 20:28:42 -0300

    SHA-256 password commented because it stop working. Try to fix it in the future

--- client/src/services/session-service.jsx
+++ client/src/services/session-service.jsx
@@ -1,5 +1,4 @@
 import { get } from './base-service';
-import jsSHA from 'jssha';
 
 const BASE_URL = 'session';
 
@@ -8,12 +7,13 @@ export async function getSessionInfo() {
 }
 
 export async function logIn(user, password) {
-  const shaObj = new jsSHA('SHA-256', 'TEXT', { encoding: 'UTF8' });
-  shaObj.update(password);
-  const hashedPassword = shaObj.getHash('HEX');
+  // const shaObj = new jsSHA('SHA-256', 'TEXT', { encoding: 'UTF8' });
+  // shaObj.update(password);
+  // const hashedPassword = shaObj.getHash('HEX');
 
   return get(BASE_URL + '/login', {
-    params: { user, password: hashedPassword },
+    // params: { user, password: hashedPassword },
+    params: { user, password },
   });
 }
 
